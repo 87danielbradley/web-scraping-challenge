8b984e5847d50b99d1da17a94ae70ae625108b39
Daniel Bradley <87danielbradley@gmail.com>
2020-05-12 23:07:07 -0400

    Final Update

--- Mission_to_Mars/app.py
+++ Mission_to_Mars/app.py
@@ -1,6 +1,6 @@
 #import necessary libraries
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+from flask_pymongo import pymongo
 # import pymongo
 import scrape_mars
 
@@ -8,26 +8,28 @@ import scrape_mars
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
+#mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
 
 # Initialize PyMongo to work with MongoDBs
-# conn = 'mongodb://localhost:27017'
-# client = pymongo.MongoClient(conn)
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+
+mars_db = client.mars
 
 
 #create route that renders index.html template
 @app.route("/")
 def home():
     # Find one record of data from the mongo database
-    mars_data = mongo.db.mars.find_one()
+    mars_data = mars_db.output.find()
     return render_template("index.html", mars_data=mars_data)
 
 @app.route("/scrape")
 def scrape():
-    #mars_db = mongo.db.mars
+    mars_db.output.drop()
     mars_data = scrape_mars.scrape()
-    mongo.db.mars.update({}, mars_data, upsert=True)
+    mars_db.output.insert_one(mars_data)
     return redirect('/')
 
 if __name__  == "__main__":
